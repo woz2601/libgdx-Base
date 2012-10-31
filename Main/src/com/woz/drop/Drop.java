d2173e114e39fc39241a926cd48fd9455cbce79e
Daniel Woznica <woz2601@gmail.com>
2012-10-31 00:00:45 -0400

    instantiated assets in code

--- Main/src/com/woz/drop/Drop.java
+++ Main/src/com/woz/drop/Drop.java
@@ -1,22 +1,94 @@
 package com.woz.drop;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.AudioDevice;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created: Woz
  * Date: 10/30/12 *
  */
 
-public class Drop extends Game
+public class Drop implements ApplicationListener
 {
+    Texture dropImage;
+    Texture bucketImage;
+    
+    Sound dropSound;
+    Music rainMusic;
+
+    OrthographicCamera camera;
+    SpriteBatch batch;
+
+    Rectangle bucket;
+    
     @Override
-    public void create()
+    public void create() 
     {
-        //This is a test comment
-        //This is a second test comment
+        dropImage = new Texture(Gdx.files.internal("droplet.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        rainMusic.setLooping(true);
+        rainMusic.play();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true);
+
+        batch = new SpriteBatch();
+
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 48 / 2;
+        bucket.y = 20;
+        bucket.width = 48;
+        bucket.height = 48;
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void render()
+    {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.end();
+
+    }
+
+    @Override
+    public void pause() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void resume() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void dispose() {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 
 
